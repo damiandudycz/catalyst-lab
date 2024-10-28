9cad129de8281b65942c4af72ab17359c7f9f0e4
Damian Dudycz <damiandudycz@yahoo.com>
2024-10-28 19:24:00 +0000

    Automatically rebuild world when changing profile from parent

--- templates/ps3/23.0-default/stage4-desktop-openrc/fsscript.sh
+++
@@ -1,6 +0,0 @@
-# When switching to different profile we need to rebuild world in fsscript.
-# Without this old packages are not updated in stage4, as update_seed doesn't work in this stage.
-
-emerge --changed-use --update --deep --usepkg --buildpkg --quiet --with-bdeps=y @world
-emerge  --depclean
-revdep-rebuild
